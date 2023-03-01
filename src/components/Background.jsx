4d495d698552d3cbab6ed244a5720a86347f1aee
Grumpy Bamba <andrejbabamov@gmail.com>
2023-03-01 15:26:43 +0100

    Added mobile support

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ import Video from '../media/background.mp4';
 const Background = () => {
     return (
         <div className='Background'>
-            <video autoPlay muted loop>
+            <video autoPlay muted loop playsinline>
                 <source src={Video} type='video/mp4'/>
             </video>
         </div>
